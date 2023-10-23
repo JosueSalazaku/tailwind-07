c0691f3c324609ae81f5059f67a6f546b1f3e7fe
Josué Salazaku <josuesalazaku@gmail.com>
2023-10-23 11:01:54 +0200

    Improve first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    mode: "jit",
+    content: [],
+    theme: {
+        extend: {
+            height: {
+                100: "26rem",
+            },
+            width: {
+                70: "17rem",
+            },
+        },
+    },
+    plugins: [],
+};
